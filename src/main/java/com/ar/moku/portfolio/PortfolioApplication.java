729476a0ce332d10f4daa1b624e5f113e2c43929
Mónica <mnk_17@hotmail.com>
2023-01-03 15:37:40 -0300

    dockerfile agregado

--- src/main/java/com/ar/moku/portfolio/PortfolioApplication.java
+++ src/main/java/com/ar/moku/portfolio/PortfolioApplication.java
@@ -19,7 +19,8 @@ public class PortfolioApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontend-angular-moku-17.web.app","https://frontend-angular-moku-17.firebaseapp.com","https://portfolio.moku.com.ar"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+//        corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontend-angular-moku-17.web.app","https://frontend-angular-moku-17.firebaseapp.com","https://portfolio.moku.com.ar"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
